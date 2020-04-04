a8f217aae88fcedd0aff9c5d6d92338f52b135be
Harrison R. Lara <harrisonlara97@gmail.com>
2020-04-03 23:35:26 -0500

    create deployment

---
+++ node-deploy/src/index.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+const AWS = require('aws-sdk');
+const child_process = require('child_process');
+const { format } = require('date-fns');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const deploymentDir = process.argv[2];
+const deploymentDirName = path.basename(deploymentDir);
+
+const rel = relPath => path.resolve(deploymentDir, relPath);
+
+const tfFilePath = rel('../terraform/terraform.tfstate');
+
+if (!fs.existsSync(tfFilePath)) {
+    throw new Error("terraform state file does not exist. Run `Terraform Apply`")
+}
+
+const { outputs } = JSON.parse(fs.readFileSync(tfFilePath, "utf-8"));
+
+require('dotenv').config({ path: rel('./.deploy.env') });
+
+const accessEnv = require('./helpers/accessEnv');
+
+const exec = util.promisify(child_process.exec);
+
+const getFullDate = () => format(new Date(), 'yyyyMMddHHmmss');
+
+const APPLICATION_NAME = accessEnv('APPLICATION_NAME');
+
+const MAX_BUFFER_SIZE = 1024 * 1024;
+
+const awsRegion = outputs["aws-region"].value;
+
+const codeDeployClient = new AWS.CodeDeploy({
+    accessKeyId: accessEnv("AWS_ACCESS_KEY_ID"),
+    apiVersion: '2014-10-06',
+    region: awsRegion,
+    secretAccessKey: accessEnv("AWS_ACCESS_KEY_SECRET")
+});
+
+const s3Client = new AWS.CodeDeploy({
+    accessKeyId: accessEnv("AWS_ACCESS_KEY_ID"),
+    endpoint: new AWS.Endpoint(`https://s3.${awsRegion}.amazonaws.com/`),
+    s3ForcePathStyle: true,
+    secretAccessKey: accessEnv("AWS_ACCESS_KEY_SECRET")
+});
+
+const rootDir = rel('../');
+
+(async () => {
+    console.log('Deploying in 3 seconds...');
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    const lockFilePath = rel('../deploy.lock');
+    if (fs.existsSync(lockFilePath)) {
+        console.error('Lockfile deploy.lock found! Stopping...');
+        process.exit();
+    }
+
+    console.log('Creating lockfile...');
+    fs.writeFileSync(lockFilePath, 'This stop node-deploy from running concurrently with itself.');
+
+    console.log('Checking environment');
+    if (!fs.existsSync(rel('.production.env'))) {
+        console.log('No .production.env found! Stopping...')
+        process.exit();
+    }
+
+    console.log('Copying appspec...');
+    fs.copyFileSync(rel('./appspec.yml'), rel('../appspec.yml'));
+
+    console.log('Generating deployment file');
+    const filename = `${deploymentDirName}-deployment-${getFullDate}.zip`;
+    const zipPath = `/tmp/${filename}`;
+    await exec(
+        `zip -r ${zipPath} . -x terraform/\\* -x node_modules/\\* -x \\*/node_modules/\\ -x \\*/.cache/\\* -x \\*.DS_Store`,
+        { cwd: rootDir, maxBuffer: MAX_BUFFER_SIZE }
+    );
+
+    console.log('Uploading deployment file');
+    await s3Client
+        .putObject({
+            Body: fs.createReadStream(zipPath),
+            Bucket: outputs[`${APPLICATION_NAME}-deployment-bucket-name`].value,
+            Key: filename
+        }).promise();
+
+    console.log('Upload complete! Deployment file is:', filename);
+
+    console.log('Deploying application');
+
+    await codeDeployClient.createDeployment({
+        applicationName: outputs[`${APPLICATION_NAME}-codedeploy-app-name`].value,
+        deploymentGroupName: accessEnv('CODEDEPLOY_DEPLOYMENT_GROUP_NAME'),
+        revision: {
+            revisionType: 'S3',
+            s3Location: {
+                bucket: outputs[`${APPLICATION_NAME}-deployment-bucket-name`].value,
+                bundleType: 'zip',
+                key: filename
+            }
+        }
+    }).promise();
+
+    console.log('Deployment initiated on CodeDeploy');
+
+    console.log('Cleaning up...');
+    fs.unlinkSync(rel('../deploy.lock'));
+    fs.unlinkSync(rel('../appspec.yml'));
+})();
---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "api-gateway-private-ip" {
+  value = module.api-gateway.private-ip
+}
+
+output "api-gateway-public-ip" {
+  value = aws_eip.api-gateway-eip.public_ip
+}
+
+output "aws-region" {
+  value = var.aws-region
+}
