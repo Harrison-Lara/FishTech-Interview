91b0ad120153ef2f82112edf993661b9c176764a
Harrison R. Lara <harrisonlara97@gmail.com>
2020-04-03 22:49:53 -0500

    setup api-gateway

---
+++ api-gateway/src/server/server.js
@@ -0,0 +1,25 @@
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import express from "express";
+import { accessEnv } from "../helpers/accessEnv";
+
+const PORT = accessEnv("PORT", 7000);
+
+const apolloServer = new ApolloServer({
+    context: a => a
+});
+
+const app = express();
+
+app.use(
+    cors({
+        origin: (origin, cb) => cb(null, true),
+        credentials: true
+    })
+);
+
+apolloServer.applyMiddleware({ app, cors: false });
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.info(`API gateway listening on ${PORT}`);
+});
